7f5173fe97c409e9191949d9ae013136fabc5822
shivam <bhaveshbeladiyauwsb@gmail.com>
2023-02-21 12:21:48 +0530

    create patron app submit form

--- src/pages/gift-cards/index.js
+++ src/pages/gift-cards/index.js
@@ -28,7 +28,6 @@ import Tabs from '@mui/joy/Tabs';
 import TabList from '@mui/joy/TabList';
 import Tab from '@mui/joy/Tab';
 import TabPanel from '@mui/joy/TabPanel';
-import {makeStyles} from "@mui/styles";
 
 const GiftCards = () => {
 
