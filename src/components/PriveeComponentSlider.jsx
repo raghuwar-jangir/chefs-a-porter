06604bb448778871a81af64570ba8249efbb4a7f
hardikbeladiya <beladiyahardik7@gmail.com>
2023-03-14 11:02:50 +0530

    carousel issue solved for server in privee page

--- src/components/PriveeComponentSlider.jsx
+++ src/components/PriveeComponentSlider.jsx
@@ -1,7 +1,5 @@
-import React, { useRef, useState } from "react";
-import { Box } from "@mui/system";
-import styled from "styled-components";
-import {Typography} from "@mui/material";
+import React from "react";
+import {Box, styled, Typography} from "@mui/material";
 import image1 from '../assets/images/ig1.png';
 import image2 from '../assets/images/ig2.png';
 import image3 from '../assets/images/ig3.png';
@@ -14,9 +12,9 @@ import '../assets/styles/fontStyle.css'
 const  PriveeComponentSlider = () => {
 const WrapBox = styled(Box) ({
     '.main-box':{
-        background: '#101418',
-    padding: '80px 0px',
-    textAlign: 'center'
+        background: '#101418 !important',
+    padding: '80px 0px !important',
+    textAlign: 'center !important',
     },
     '.peek-heading':{
         fontFamily: 'Proxima Nova Alt',
